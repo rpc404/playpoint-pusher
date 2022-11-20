b174e16e2334ee487894d62e0e04bf0bd4653133
Sudeep Mishra <36982217+SudeepMi@users.noreply.github.com>
2022-11-20 18:16:25 +0000

    predict api

--- api/controllers/Prediction.js
+++ api/controllers/Prediction.js
@@ -10,10 +10,12 @@ module.exports = {
         });
     }),
     getPredictions: expressAsyncHandler(async (req, res) => {
+        const userid = req.query.userid || "";
+        const data = userid ? await Prediction.find({predictedBy:userid}) : await Prediction.find()
         res.status(200).json({
             status: "success",
             message: "Predictions fetched successfully!",
-            data: await Prediction.find()
+            data: data
         });
     }
     )
