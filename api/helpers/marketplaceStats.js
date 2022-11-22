91556fd56d010972a4ef6117423d3b877291256e
jckamaicha <theboringschool.org@gmail.com>
2022-11-22 22:10:39 +0545

    Updated Stats

--- api/helpers/marketplaceStats.js
+++ api/helpers/marketplaceStats.js
@@ -1,15 +1,17 @@
 const expressAsyncHandler = require("express-async-handler");
 const Fixture = require("../models/Fixture");
+const Prediction = require("../models/Prediction");
+const Questionaire = require("../models/Questionaire");
 
 module.exports = {
   marketplaceStats: expressAsyncHandler(async (req, res) => {
-    const query = {marketplaceSlug: req.params.marketplaceSlug};
+    const query = {marketplaceSlug: req.params["marketplaceSlug"]};
     
     const totalFixtures = await Fixture.countDocuments(query);
-    const totalQuestionaires = await Fixture.countDocuments(query);
-    const totalPredictions = await Fixture.countDocuments(query);
+    const totalQuestionaires = await Questionaire.countDocuments(query);
+    const totalPredictions = await Prediction.countDocuments(query);
 
-    res.json({
+    res.status.json({
       response: {
         totalFixtures,
         totalQuestionaires,
