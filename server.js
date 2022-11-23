e7ac3a23cc60d890696e0b1d0f14f3456a5ae360
Sudeep Mishra <36982217+SudeepMi@users.noreply.github.com>
2022-11-23 20:24:18 +0000

    pusher added

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const http = require("http").Server(app);
 
+
 const { dbConfig } = require("./utils/db");
 const APIRouter = require("./utils/router");
-const { socketConfig } = require("./utils/socket");
 const PORT = process.env.PORT || 4000;
 
 require("dotenv").config();
@@ -28,7 +28,11 @@ global.__basedir = __dirname;
  * 1. Database Configuration
  */
 dbConfig();
-socketConfig(http);
+
+
+// @dev pusher configuration
+
+
 
 /**
  * @dev Router Configuration
@@ -41,8 +45,9 @@ const apiLimiter = rateLimit({
 });
 
 app
-  .get("/", (req, res) =>
+  .get("/", (req, res) =>{
     res.json({ message: "Welcome to V1 Playpoint API! 👌" })
+  }
   )
   // this must be used for production
   // .use("/api/v1", apiLimiter, APIRouter)
