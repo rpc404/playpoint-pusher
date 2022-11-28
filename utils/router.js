bf64221b395ae3fabf78f9663a4daa195f5e6dcf
sudeepmi <sudeepm0599@gmail.com>
2022-11-28 18:55:50 +0545

    authorization

--- utils/router.js
+++ utils/router.js
@@ -32,17 +32,19 @@ const { getLeaderboards, createLeaderboard, updateLeaderboard, deleteLeaderboard
 const { setPrediction, getPredictions } = require("../api/controllers/Prediction");
 
 const APIRouter = require("express").Router();
+const { authorize } = require("../api/middlewares/authorize")
 
 // @note Marketplace API Endpoints
 APIRouter.get("/marketplace-specific/:marketplaceSlug", getSpecificMarketplace)
   .get("/marketplace", getMarketplaces)
   .post(
     "/new-marketplace",
+    authorize,
     multerUpload.single("marketplaceCoverImage"),
     newMarketplace
   )
-  .patch("/update-marketplace/:marketplaceSlug", updateMarketplace)
-  .delete("/delete-marketplace/:marketplaceSlug", deleteMarketplace);
+  .patch("/update-marketplace/:marketplaceSlug",authorize, updateMarketplace)
+  .delete("/delete-marketplace/:marketplaceSlug",authorize, deleteMarketplace);
 
 // @note Fixture API Endpoints
 APIRouter.get("/fixture", getFixturesController)
@@ -51,28 +53,28 @@ APIRouter.get("/fixture", getFixturesController)
     "/fixture-marketplace/:marketplaceSlug",
     getFixturesByMarketplaceSlugController
   )
-  .post("/new-fixture", newFixtureController)
-  .patch("/update-fixture/:id", updateFixturesController)
-  .delete("/delete-fixture/:id", deleteFixturesController);
+  .post("/new-fixture", authorize, newFixtureController)
+  .patch("/update-fixture/:id",authorize, updateFixturesController)
+  .delete("/delete-fixture/:id",authorize, deleteFixturesController);
 
 // @note Questionaires API Endpoints
 APIRouter.get("/questionaires", getQuestionaireController)
   .get("/questionaires/:fixtureId", getSpecificQuestionaireController)
-  .post("/new-questionaire", newQuestionaireController)
-  .patch("/update-questionaire/:questionaireId", updateQuestionaireController)
-  .delete("/delete-questionaire/:questionaireId", deleteQuestionaireController);
+  .post("/new-questionaire",authorize, newQuestionaireController)
+  .patch("/update-questionaire/:questionaireId",authorize, updateQuestionaireController)
+  .delete("/delete-questionaire/:questionaireId",authorize, deleteQuestionaireController);
 
 // @note Results API Endpoints
 APIRouter.get("/results", getResultController)
-  .post("/new-result", newResultController)
-  .patch("/update-result", updateResultController)
-  .delete("/delete-result", deleteResultController);
+  .post("/new-result",authorize, newResultController)
+  .patch("/update-result",authorize, updateResultController)
+  .delete("/delete-result",authorize, deleteResultController);
 
 // @note Leaderboards API Endpoints
 APIRouter.get("/leaderboards", getLeaderboards)
-.post("/leaderboards", createLeaderboard)
-.patch("/leaderboards/:leaderboardId", updateLeaderboard)
-.delete("/leaderboards/:leaderboardId", deleteLeaderboard)
+.post("/leaderboards",authorize, createLeaderboard)
+.patch("/leaderboards/:leaderboardId",authorize, updateLeaderboard)
+.delete("/leaderboards/:leaderboardId",authorize, deleteLeaderboard)
 
 // @note Active Prediction
 APIRouter.post("/prediction", setPrediction).get("/prediction", getPredictions);
