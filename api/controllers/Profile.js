7568246c6569fad417d1a878cee0ac7ae4797c6f
surajgaire14 <eriag321@gmail.com>
2022-12-05 17:45:23 +0545

    backend updated

--- api/controllers/Profile.js
+++ api/controllers/Profile.js
@@ -1,29 +1,47 @@
 const Profile = require("../models/Profile");
 const expressAsyncHandler = require("express-async-handler");
 const { sanitizeQueryInput } = require("../../utils/QuerySanitizer");
-const { uniqueNamesGenerator, adjectives, names  } = require('unique-names-generator');
-
+const {
+  uniqueNamesGenerator,
+  adjectives,
+  names,
+} = require("unique-names-generator");
 
 module.exports = {
   /**
    * @dev Get Specific Marketplaces
    */
   setProfile: expressAsyncHandler(async (req, res) => {
-    let profile = await Profile.findOne({walletID:req.body.userPublicAddress})
-    const randomName = uniqueNamesGenerator({ dictionaries: [adjectives, names] }); 
-    if(req.body.username && profile){
-        profile.username = req.body.username
-        await profile.save();
+    let profile = await Profile.findOne({
+      walletID: req.body.userPublicAddress,
+    });
+    const randomName = uniqueNamesGenerator({
+      dictionaries: [adjectives, names],
+    });
+    if (req.body.username && profile) {
+      profile.username = req.body.username;
+      await profile.save();
     }
-    if(profile && !profile.username){
-        profile.username = randomName;
-        await profile.save();  
-        profile = await Profile.findOne({walletID:req.body.userPublicAddress})
+    if (profile && !profile.username) {
+      profile.username = randomName;
+      await profile.save();
+      profile = await Profile.findOne({ walletID: req.body.userPublicAddress });
     }
-    if(!profile){
-        profile = await Profile.create({walletID:req.body.userPublicAddress,username:randomName})
+    if (!profile) {
+      profile = await Profile.create({
+        walletID: req.body.userPublicAddress,
+        username: randomName,
+      });
     }
-    res.status(200).send({profile: profile});
-  })
+    res.status(200).send({ profile: profile });
+  }),
 
-}
+  getProfile: expressAsyncHandler(async (req, res) => {
+    const profile = await Profile.find({}).count();
+    if (profile) {
+      return res.status(200).send({ profile: profile });
+    }else{
+      return res.status(500).json({msg:"profile not found!"})
+    }
+  }),
+};
